6ca78640c0ddfdacbbf675f8b0cb39624f14ba2c
mri118 <mrityunjaysinha0118@gmail.com>
2022-06-03 13:36:10 +0530

    configcode

---
+++ src/api/awardpartner/routes/awardpartner.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * awardpartner router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::awardpartner.awardpartner');
